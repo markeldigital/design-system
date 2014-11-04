585426712d2f19d88e10343a4cb3f515e85fe492
Nick Bailey on segrdev178 <test+nb@gmail.com>
2014-11-04 13:21:14 +0000

    Grunt now publishes design system for local development.

--- gruntfile.js
+++ gruntfile.js
@@ -5,6 +5,15 @@ module.exports = function (grunt) {
     console.log(projectRoot);
     var sourcePathForScss = path.join(projectRoot, "/Scss/design-system.scss");
     console.log(sourcePathForScss);
+
+    var params = {
+        'sass-publishing-path': 'design_system/Shared/Scss/',
+        'views-publishing-path': 'design_system/Shared/Views/',
+        'broker-root': '../REMUS.BrokerPortal/Markel.REMUS.BrokerPortal.Web',
+        'public-root': '../REMUS.RetailPublicSite/REMUS.RetailPublicSite',
+        'underwriter-root': '../REMUS.UnderwriterPortal/REMUS.UnderwriterPortal'
+    };
+
     grunt.initConfig({
         sass: {
             dev: {
@@ -27,14 +36,30 @@ module.exports = function (grunt) {
             },
             css: {
                 files: path.join(projectRoot, '/Scss/**'),
-                tasks: ['sass']
+                tasks: ['sass', 'copy:toProjectsforLocalDev']
+            }
+        },
+
+        copy: {
+            toProjectsforLocalDev: {
+                files: [
+                    // Broker
+                    {expand: true, src: ['Sass/**'], dest: params['broker-root'] + params['sass-publishing-path']},
+                    {expand: true, src: ['Views/**'], dest: params['broker-root'] + params['views-publishing-path']},
+                    // Public
+                    {expand: true, src: ['Sass/**'], dest: params['public-root'] + params['sass-publishing-path']},
+                    {expand: true, src: ['Views/**'], dest: params['public-root'] + params['views-publishing-path']},
+                    // Underwriter
+                    {expand: true, src: ['Sass/**'], dest: params['underwriter-root'] + params['sass-publishing-path']},
+                    {expand: true, src: ['Views/**'], dest: params['underwriter-root'] + params['views-publishing-path']}
+                ]
             }
         }
-
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.registerTask('default', ['watch']);
 
 };
