49350dab211756c9480df8e2bf552fdd194a4a2e
Nick Bailey <tw.at.mrk+nb@gmail.com>
2015-03-11 17:42:30 +0000

    C1180: Mobile vs. desktop tabs now included in design system

--- Markel.REMUS.DesignSystem.Web/Components/Tabs/js/ds-tabs-feature-management-liability.js
+++ Markel.REMUS.DesignSystem.Web/Components/Tabs/js/ds-tabs-feature-management-liability.js
@@ -3,33 +3,55 @@
 
     var prefix = ds.prefix + '-tab';
     var selectors = {
-        tabsSelector: prefix + "s-management-liability",
+        tabsModuleClass: prefix + "s-management-liability",
         triggerClass: prefix + '-trigger',
-        tabContentClass: prefix + '-content',
-        triggerCurrentClass: prefix + '-trigger--current',
-        closedClass: prefix + '-closed',
-        openClass: prefix + '-open'
+        tabContentClass: prefix + '-content'
     };
 
     scope.desktopTabs = (function(){
         var scope2 = {};
-        scope2.makeDefaultTabActiveOnPageLoad = function(tabsSelector, tabTriggerSelector, tabContentSelector) {
-            var tabTriggers = document.querySelectorAll(tabsSelector + " " + tabTriggerSelector);
-            var tabContentContainers = document.querySelectorAll(tabsSelector + " " + tabContentSelector);
 
-            for (var i = 0; i < tabTriggers.length; i++) {
-                if (tabTriggers[i].classList.contains('ds-tab-trigger--current')) {
-                    tabContentContainers[i].style.display = "block";
-                }
-            }
+        scope2.findFirstTabsModule = function(){
+            return document.querySelectorAll('.' + selectors.tabsModuleClass)[0];
         };
 
-        scope2.hideAllTabs = function(tabContentContainers, tabTriggers) {
-            for (var i = 0; i < tabContentContainers.length; i++) {
-                tabContentContainers[i].style.display = "none";
-            }
-            for (var j = 0; j < tabTriggers.length; j++) {
-                tabTriggers[j].classList.remove("ds-tab-trigger--current");
+        scope2.findAllTabs = function(){
+            return scope2.findFirstTabsModule().querySelectorAll('.' + selectors.triggerClass);
+        };
+
+        scope2.findTabNumber = function(tabNumber){
+            return scope2.findFirstTabsModule().querySelectorAll('.' + selectors.triggerClass)[tabNumber];
+        };
+
+        scope2.findTabContentForTabNumber = function(tabNumber){
+            return scope2.findFirstTabsModule().querySelectorAll('.' + selectors.tabContentClass)[tabNumber];
+        };
+
+        scope2.hideTabNumber = function(tabNumber){
+            scope2.findTabNumber(tabNumber).classList.remove('ds-tab-trigger--current');
+            scope2.findTabContentForTabNumber(tabNumber).classList.remove('ds-tab-content--current');
+        };
+
+        scope2.showTabNumber = function(tabNumber){
+            scope2.findTabNumber(tabNumber).classList.add('ds-tab-trigger--current');
+            scope2.findTabContentForTabNumber(tabNumber).classList.add('ds-tab-content--current');
+        };
+
+        scope2.showTab = function(tab){
+            tab.classList.add('ds-tab-trigger--current');
+            var contentToShowSelector = tab.getAttribute('href');
+            var contentToShow = document.querySelector(contentToShowSelector);
+            contentToShow.classList.add('ds-tab-content--current');
+        };
+
+        scope2.makeFirstTabActive = function() {
+            scope2.showTabNumber(0);
+        };
+
+        scope2.hideAllTabs = function() {
+            var tabTriggers = scope2.findAllTabs();
+            for (var i = 0; i < tabTriggers.length; i++) {
+                scope2.hideTabNumber(i);
             }
         };
 
@@ -42,31 +64,23 @@
 
         scope2.setTabAsActive = function(e) {
             var clickedElement = e.target || e.srcElement;
-            var clickedTab = findClickedTabInParents(clickedElement);
-            clickedTab.classList.add('ds-tab-trigger--current');
-            var contentToShowSelector = clickedTab.getAttribute('href');
-            var contentToShow = document.querySelector(contentToShowSelector);
-            contentToShow.style.display = "block";
+            var clickedTab = scope2.findClickedTabInParents(clickedElement);
+            scope2.showTab(clickedTab);
         };
 
-        scope2.switchTabs = function(e, tabsSelector, tabTriggerSelector, tabContentSelector) {
-            var tabTriggers = document.querySelectorAll(tabsSelector + " " + tabTriggerSelector);
-            var tabContentContainers = document.querySelectorAll(tabsSelector + " " + tabContentSelector);
-
-            hideAllTabs(tabContentContainers, tabTriggers);
-            setTabAsActive(e);
+        scope2.toggleTab = function(e, tabsModuleClass, tabTriggerClass, tabContentClass) {
+            scope2.hideAllTabs();
+            scope2.setTabAsActive(e);
         };
 
-        scope2.setupTabsClickHandlers = function(tabsSelector, tabTriggerSelector, tabContentSelector) {
-            for (var i = 0; i < document.querySelectorAll(tabTriggerSelector).length; i++) {
-                document.querySelectorAll(tabsSelector + " " + tabTriggerSelector)[i].addEventListener('click', function (e) {
-                    if (e.preventDefault) {
-                        e.preventDefault();
-                    } else {
-                        e.returnValue = false; // IE8 fallback
-                    }
-                    switchTabs(e, tabsSelector, tabTriggerSelector, tabContentSelector);
-                });
+        scope2.handleClick = function (e) {
+            e.preventDefault ? e.preventDefault() : e.returnValue = false /* IE8 fallback */;
+            scope2.toggleTab(e, selectors.tabsModuleClass, selectors.triggerClass, selectors.tabContentClass);
+        };
+
+        scope2.setupTabsClickHandlers = function() {
+            for (var i = 0; i < scope2.findAllTabs().length; i++) {
+                scope2.findTabNumber(i).addEventListener('click', scope2.handleClick);
             }
         };
 
@@ -77,21 +91,30 @@
     scope.mobileExpanders = (function(){
         var scope2 = {};
 
-        scope2.setupClickHandlers = function(tabsSelector, tabTriggerSelector, tabContentClass, openClass, closedClass){
-            for (var i = 0; i < document.querySelectorAll(tabTriggerSelector).length; i++) {
-                document.querySelectorAll(tabsSelector + " " + tabTriggerSelector)[i].addEventListener('click', function (e) {
-                    if (e.preventDefault) {
-                        e.preventDefault();
-                    } else {
-                        e.returnValue = false; // IE8 fallback
-                    }
-                    var tabContentElement = e.parentNode.querySelector(tabContentClass);
-                    if(tabContentElement.classList.contains(openClass)){
-                        tabContentElement.classList.add(closedClass);
-                    } else {
-                        tabContentElement.classList.add(openClass);
-                    }
-                });
+        var openClass = 'ds-palm-tabs--open';
+        var trigger = 'ds-tab-trigger';
+        var triggeredContent = 'ds-tab-content';
+
+        scope2.toggleExpander = function(tabTrigger){
+            var tabContentElement = tabTrigger.parentNode.querySelector('.' + triggeredContent);
+            if(tabTrigger.classList.contains(openClass)){
+                tabTrigger.classList.remove(openClass);
+                tabContentElement.classList.remove(openClass);
+            } else {
+                tabTrigger.classList.add(openClass);
+                tabContentElement.classList.add(openClass);
+            }
+        };
+
+        scope2.handleClick = function(e){
+            e.preventDefault ? e.preventDefault() : e.returnValue = false /* IE8 fallback */;
+            scope2.toggleExpander(e.currentTarget);
+        };
+
+        scope2.setupClickHandlers = function(tabsModuleClass, tabTriggerClass){
+            for (var i = 0; i < document.querySelectorAll('.' + tabTriggerClass).length; i++) {
+                document.querySelectorAll('.' + tabsModuleClass + " " + '.' + tabTriggerClass)[i].removeEventListener('click', scope2.handleClick); // remove any pre-existing events
+                document.querySelectorAll('.' + tabsModuleClass + " " + '.' + tabTriggerClass)[i].addEventListener('click', scope2.handleClick);
             }
         };
 
@@ -99,11 +122,12 @@
     })();
 
     scope.setup = function(){
+        // TODO: Use isolating module selector to limit effect on other pages when we start using this more generally...
         ds.respondToPalmVsLapAndDesk(function(){
-            //scope.mobileExpanders.setupClickHandlers(selectors.tabsSelector, selectors.triggerClass, selectors.tabContentClass, selectors.openClass, selectors.closedClass);
+            scope.mobileExpanders.setupClickHandlers(selectors.tabsModuleClass, selectors.triggerClass);
         }, function(){
-            //scope.desktopTabs.makeDefaultTabActiveOnPageLoad(selectors.tabsSelector, selectors.triggerClass, selectors.tabContentClass);
-            //scope.desktopTabs.setupTabsClickHandlers(selectors.tabsSelector, selectors.triggerClass, selectors.tabContentClass);
+            scope.desktopTabs.makeFirstTabActive(selectors.tabsModuleClass, selectors.triggerClass, selectors.tabContentClass);
+            scope.desktopTabs.setupTabsClickHandlers(selectors.tabsModuleClass, selectors.triggerClass, selectors.tabContentClass);
         });
     };
 
@@ -111,5 +135,8 @@
 })();
 
 window.addEventListener("load", function () {
+    ds.tabsFeatureManagementLiability.setup();
+});
+window.addEventListener("resize", function () {
     ds.tabsFeatureManagementLiability.setup();
 });
