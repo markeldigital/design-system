52afab27d336fe5a546a23873b6d80e9c16b6919
Robin Weston and Natalie Eskinazi on segrdev187 <tw.at.mrk+rw+ne@gmail.com>
2015-06-29 12:04:36 +0100

    C1540: Put the polyfill back in :/

--- Components/main-brand/menu/js/menu.js
+++ Components/main-brand/menu/js/menu.js
@@ -1,6 +1,20 @@
 ds.menu = (function(keyboard, openClass, closedClass){
     var scope = {};
 
+    this.addEventListener('focus', function (event) {
+        event.target.dispatchEvent(new Event('focusin', {
+            bubbles: true,
+            cancelable: true
+        }));
+    }, true);
+
+    this.addEventListener('blur', function (event) {
+        event.target.dispatchEvent(new Event('focusout', {
+            bubbles: true,
+            cancelable: true
+        }));
+    }, true);
+
     var prefix = ds.prefix + '-menu';
     var selectors = {
         menuDropdownClass: prefix,
